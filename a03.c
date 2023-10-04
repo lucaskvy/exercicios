54207e03b139360900bdd76274a8a81206e9b87b
kavay1 <lucas.souzab@sempreceub.com>
2023-10-04 18:43:08 -0300

    exercicio

--- a03.c
+++ a03.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
-int maincx
+int main(){
+
+    float peso;
+    float altura;
+
+    printf("Qual seu peso? ");
+    scanf("%f", &peso);
+
+    printf("Qual sua altura? ");
+    scanf("%f", &altura);
+
+    float imc = peso/pow(altura,2);
+
+    printf("%f", imc);
+
+}
